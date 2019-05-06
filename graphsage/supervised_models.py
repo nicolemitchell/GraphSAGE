05620a71fb61c9ca36b5fe1f5095308cc78e449d
Nicole Mitchell <nem3@rice.edu>
2019-05-06 18:25:17 -0500

    debugging

--- graphsage/supervised_models.py
+++ graphsage/supervised_models.py
@@ -111,8 +111,8 @@ class SupervisedGraphsage(models.SampleAndAggregate):
         if self.sigmoid_loss:
             self.loss += tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(
                     logits=self.node_preds,
-                    targets=self.placeholders['labels']),
-                    pos_weights=9.4)
+                    targets=self.placeholders['labels'],
+                    pos_weights=9.4))
         else:
             self.loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
                     logits=self.node_preds,
