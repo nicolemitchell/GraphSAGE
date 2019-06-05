c723cd7db6ff33a0b902c7d6cf5cc2e82ccaadec
Nicole Mitchell <nem3@rice.edu>
2019-06-05 10:08:22 -0500

    removed weighting on positive examples

--- graphsage/supervised_models.py
+++ graphsage/supervised_models.py
@@ -112,7 +112,7 @@ class SupervisedGraphsage(models.SampleAndAggregate):
             self.loss += tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(
                     logits=self.node_preds[:,1],
                     targets=self.placeholders['labels'][:,1],
-                    pos_weight=9.4))
+                    pos_weight=1))
         else:
             self.loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
                     logits=self.node_preds,
